72290c5aec581c9bf68a5a39f0f4509625997d5e
ugochukwudev <paulambrose5002@gmail.com>
2024-09-09 19:56:09 +0100

    feat: updated about us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import Tilt from "react-tilt";
+import {Tilt} from "react-tilt";
 import { motion } from "framer-motion";
 import AOS from "aos";
 import "aos/dist/aos.css";
